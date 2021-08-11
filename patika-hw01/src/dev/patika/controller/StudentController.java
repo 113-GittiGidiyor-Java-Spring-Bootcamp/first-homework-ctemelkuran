75c5aeae9cd41e5b78672e0b75958cc6c089aba9
Cigir Temelkuran <91200000641@ogrenci.ege.edu.tr>
2021-08-11 21:15:19 +0300

    commented

---
+++ patika-hw01/src/dev/patika/controller/StudentController.java
@@ -0,0 +1,18 @@
+package dev.patika.controller;
+
+import dev.patika.models.Student;
+import dev.patika.service.StudentService;
+
+import java.util.*;
+
+public class StudentController {
+    private StudentService studentService = new StudentService();
+
+    /*public Student findStudent(int studId){
+        return studentService.findById(studId);
+    }
+
+    public List<Student> findAllStudents(){
+        return StudentService.findAll();
+    }*/
+}
---
+++ patika-hw01/src/dev/patika/service/StudentService.java
@@ -0,0 +1,4 @@
+package dev.patika.service;
+
+public class StudentService {
+}
